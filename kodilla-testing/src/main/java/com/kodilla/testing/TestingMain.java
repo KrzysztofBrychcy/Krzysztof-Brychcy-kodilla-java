bd8b8975b1b27eb28f4a0977f54e8b4a22784fed
KrzysztofBrychcy <brychcy.krzysztof@gmail.com>
2018-11-12 12:06:53 +0100

    Modul 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,30 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String args[]){
-        System.out.println("Moduł6. Wporwadzenie do testowania modułowego.");
+
+        System.out.println("Moduł6.2. Sprawdzenie Klasy Calculator");
+
+        System.out.println("");
+        System.out.print("Dodawanie:\t");
+
+        Calculator kalkulator = new Calculator();
+        if (kalkulator.add(4,13) == (4 + 13)){
+            System.out.println("Test OK");
+        }else {
+            System.out.println("Error!");
+        }
+
+        System.out.print("Odejmanie:\t");
+
+        if(kalkulator.subtract(45,76) == (45 - 76)){
+            System.out.println("Test OK");
+        }else {
+            System.out.println("Error!");
+        }
+
+
     }
 }

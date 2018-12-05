3834f83d91bd6a05a5926cf14c2784275ed1bf73
KrzysztofBrychcy <brychcy.krzysztof@gmail.com>
2018-12-05 15:53:27 +0100

    Moduł 8.3

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,4 +1,11 @@
 package com.kodilla.exception.test;
 
 public class ExceptionHandling extends Exception {
+    public ExceptionHandling(){
+        super();
+        System.out.println("Problem with Handing 1");
+    }
+    public ExceptionHandling(Throwable cause){
+        super(cause);
+    }
 }

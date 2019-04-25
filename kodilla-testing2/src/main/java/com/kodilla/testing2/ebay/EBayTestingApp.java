84fa6cff53d35e2c5c5b700df22901d3cd71d112
KrzysztofBrychcy <brychcy.krzysztof@gmail.com>
2019-04-25 16:52:58 +0200

    Moudł23.2

--- kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
@@ -6,14 +6,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class EBayTestingApp {
-    public static final String SEARCHFILED = "gh-tb";
+    public static final String SEARCHFILED = "gh-ac";
 
     public static void main(String[] args) {
 
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         driver.get("https://www.ebay.com/");
 
-        WebElement searchField = driver.findElement(By.name(SEARCHFILED));
+        WebElement searchField = driver.findElement(By.id(SEARCHFILED));
         searchField.sendKeys("Laptop");
         searchField.submit();
     }
